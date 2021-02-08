3b7c28fcc33f7a5703b9789b445bded6fe9d0ea3
Nico Albanese <gcalbanese96@gmail.com>
2021-02-08 18:46:30 +0000

    testing git

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,7 @@
   <div id="inner-wrapper" class="h-full flex items-center justify-center flex-col">
     <div id="header" class="text-2xl font-bold mb-4 animate-pulse">🚀 site in progress 🚀</div>
     <div id="find-twitter">
-      find me on <a class="text-gray-200 hover:text-gray-400 underline transition" href="https://twitter.com/nicoalbanese10">Twitter</a>
+      find me on (test) <a class="text-gray-200 hover:text-gray-400 underline transition" href="https://twitter.com/nicoalbanese10">Twitter</a>
     </div>
   </div>
 </div>
