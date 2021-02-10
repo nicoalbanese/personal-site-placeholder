fdecac529565bf7da6b29cd8f56a94a74e93e0af
Nico Albanese <gcalbanese96@gmail.com>
2021-02-10 13:41:23 +0000

    new design

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,64 @@
 <div id="app-wrapper" class="h-screen w-full bg-gray-800 text-white">
-  <div id="inner-wrapper" class="h-full flex items-center justify-center flex-col">
-    <div id="header" class="text-2xl font-bold mb-4 animate-pulse">🚀 site in progress 🚀</div>
-    <div id="find-twitter">
-      find me on <a class="text-gray-200 hover:text-gray-400 underline transition" href="https://twitter.com/nicoalbanese10">Twitter</a>
+  <div
+    id="inner-wrapper"
+    class="h-full flex items-center justify-center flex-col"
+  >
+    <div
+      id="main-card"
+      class="bg-gray-100 sm:p-8 py-8 text-gray-700 rounded-xl shadow-2xl flex flex-col max-w-3xl sm:mx-0 mx-10"
+    >
+      <div
+        id="card-top"
+        class="flex-1 flex sm:flex-row text-center sm:text-left flex-col border-b border-gray-200 pb-4 items-center"
+      >
+        <div id="profile-area" class="sm:mr-10 sm:w-1/4 w-1/2">
+          <img
+            src="/assets/profile-photo.png"
+            alt="nico-profile"
+            class="rounded-lg mb-2 shadow-md"
+          />
+        </div>
+        <div id="desription-and-welcome" class="w-3/4">
+          <h2 class="text-2xl font-bold mb-4 mt-4 sm:mt-0">
+            👋 Hi, I'm Nico! 👋
+          </h2>
+          <p>
+            I'm an investor at <a href="https://www.ascensionventures.com"
+              >Ascension</a
+            >
+            where I focus on pre-seed investments. I also help run the
+            <a href="https://debutsessions.co.uk">Debut Sessions</a>, a virtual
+            pitch initiative aiming to democratise fundraising for pre-Seed
+            startups..
+          </p>
+          <p class="my-2">
+            Aside from work, I'm an avid football fan (COYG) and an amateur
+            developer (trying my hand at full-stack at the moment). I also love
+            and <a
+              href="https://open.spotify.com/artist/6qJuCe1wWGMYQx0LFblRKR?si=P7Q8UFzSRMKNQwAjVvFZ9Q"
+            >
+              used to make music</a
+            > and always love to talk music-tech.
+          </p>
+        </div>
+      </div>
+      <div id="social-section" class="mt-2 flex-1 flex flex-col items-center">
+        <h2 class="font-bold">find me on</h2>
+        <div id="socials" class="mt-2">
+          <a class="pr-2" href="https://twitter.com/nicoalbanese10">Twitter</a>
+          <a
+            class="px-2 border-r border-l border-gray-300"
+            href="https://www.linkedin.com/in/nicoalbanese/">LinkedIn</a
+          >
+          <a class="pl-2" href="mailto:nico@ascensionventures.com">Email</a>
+        </div>
+      </div>
     </div>
   </div>
 </div>
+
+<style>
+  a {
+    @apply underline hover:text-gray-400 transition;
+  }
+</style>
